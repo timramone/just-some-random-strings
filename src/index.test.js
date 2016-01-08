b41831a5f156dbf9df61192dcabec28449eb1f31
Tim <timramone@gmail.com>
2016-01-08 23:30:38 +0300

    Added tests support

---
+++ src/index.test.js
@@ -0,0 +1,8 @@
+var randomStrings = require('./index')
+var expect = require('chai').expect;
+
+describe('random-strings', () => {
+  it('should work!', () => {
+    expect(true).to.be.true;
+  })
+})
