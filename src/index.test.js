a3d152a4f5e63cd0ad9b6c75be4b0c8b610b00e6
Tim <timramone@gmail.com>
2016-01-08 23:49:06 +0300

    Added some tests

--- src/index.test.js
+++ src/index.test.js
@@ -2,7 +2,33 @@ var randomStrings = require('./index')
 var expect = require('chai').expect;
 
 describe('random-strings', () => {
-  it('should work!', () => {
-    expect(true).to.be.true;
+  describe('all', () => {
+    it('should be an array of strings', () => {
+      var isArrayOfStrings =
+        strings =>
+          strings.every(item => typeof item === 'string');
+
+      expect(randomStrings.all).to.satisfy(isArrayOfStrings);
+    });
+
+    it('should contain 16', () => {
+      expect(randomStrings.all).
+        to.
+        include('16');
+    });
+
+    it('should not contain 13', () => {
+      expect(randomStrings.all).
+        to.
+        not.
+        include('13');
+    });
+  })
+
+  describe('random', () => {
+    it('should return random item', () => {
+      var randomItem = randomStrings.random();
+      expect(randomStrings.all).to.include(randomItem);
+    })
   })
 })
